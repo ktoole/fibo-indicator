b022a9b721ce2d4ea5d32d9f1f562ff2a51f9490
Kristian <kristiantoole@live.com>
2019-02-28 22:46:40 -0500

    Adding halfing logic in order to keep process from stopping early, still need to fix dup issue

--- Src/CalculateFibo.py
+++ Src/CalculateFibo.py
@@ -6,11 +6,6 @@ from enum import Enum
 from FiboTrends import Fibo
 import csv
 
-""" Get Data """
-df = pd.read_csv('../Data/GCG20_Monthly.csv', engine='python', skipfooter=1)
-df['Date Time'] = pd.to_datetime(df['Date Time']) # Convert all dates to datetime objects
-
-
 """ Set up variables """
 EMPTY = ""
 swingIsLowestLow = True
@@ -24,15 +19,30 @@ letterIndex = 0
 numberIndex = 1
 fiboSummary = []
 trend = EMPTY
-MONTHLY = 260
-WEEKLY = 100
+tenYearsInWeeks = 521
+halfTheWindow = 2
+csvName = ""
+foundDuplicate = False
+
+""" Get Data """
+df = pd.read_csv('../Data/GCG19_Weekly.csv', engine='python', skipfooter=1)
+df['Date Time'] = pd.to_datetime(df['Date Time']) # Convert all dates to datetime objects
+
+twentyOneYearsInMonths = 260
+fivePointFiveYearsInWeeks = 286
 DAILY = EMPTY
-numberOfCandlesToView = MONTHLY  #100 -> weekly, N/A -> daily  TODO: More testing needed for weekly and daily
+numberOfCandlesToView = fivePointFiveYearsInWeeks  #100 -> weekly, N/A -> daily  TODO: More testing needed for weekly and daily
 
 
 startIndex = 0
 endIndex = startIndex + numberOfCandlesToView
 lastIndex = df['Date Time'].size - 1
+
+TIMEFRAME = 'WEEKLY'
+
+if (TIMEFRAME == 'WEEKLY'):
+        startIndex = lastIndex - tenYearsInWeeks
+        endIndex = startIndex + numberOfCandlesToView
 
 
 """ Creating functions """
@@ -45,12 +55,16 @@ addTerminalSpace(1)
 
 """ Main Function """
 while(running):
-        
+
         if (( startIndex + numberOfCandlesToView) >= lastIndex):
-                endIndex = lastIndex
+
+                numberOfCandlesToView = numberOfCandlesToView // halfTheWindow
+                if (numberOfCandlesToView <= 1):
+                        endIndex = lastIndex
+
         else:
                 endIndex = startIndex + numberOfCandlesToView
-                
+
         # Create time window to view data
         dfTimeWindow = df.iloc[startIndex:endIndex]
 
@@ -59,38 +73,35 @@ while(running):
                 lowestLowIndex = dfTimeWindow['Low'].idxmin()
                 rowOfLowestLow = dfTimeWindow.loc[lowestLowIndex]
                 startIndex = lowestLowIndex
-                swingIsLowestLow = False       
+                swingIsLowestLow = False
         else:
                 highestHighIndex = dfTimeWindow['High'].idxmax()
                 rowOfHighestHigh = dfTimeWindow.loc[highestHighIndex]
                 startIndex = highestHighIndex
-                swingIsLowestLow = True     
+                swingIsLowestLow = True
 
         if((len(rowOfLowestLow)>0) and (len(rowOfHighestHigh)>0)):
 
-                # Perform Fibo Calculations                  
+                # Perform Fibo Calculations
                 fiboPoint = Fibo(rowOfHighestHigh['High'], rowOfLowestLow['Low'])
                 isTrendUp = fiboPoint.isTrendUp(rowOfHighestHigh, rowOfLowestLow)
                 fiboPercents = fiboPoint.calculateFiboForTrend(isTrendUp)
 
-                # Set name of trend      
-                # 
+                # Set name of trend
                 if (letterIndex > len(trendLetterNames) - 1):
-                        letterIndex = 0              
+                        letterIndex = 0
                 if (isTrendUp):
                         trend = 'UP'
                         name = trendLetterNames[letterIndex]
                         letterIndex += 1
-
                 else:
                         trend = 'DOWN'
                         name = numberIndex
                         numberIndex += 1
 
-                
                 fiboPoint.setName(name)
 
-                # Prepare Fibo info for JSON Storage              
+                # Prepare Fibo info for CSV Storage
                 fiboInfo = {
                         "Name": name,
                         "Trend": trend,
@@ -104,6 +115,16 @@ while(running):
                         "127%":fiboPercents[3],
                         "162%":fiboPercents[4]
                 }
+                # # Check for duplicate TODO:  FIX DUPLICATE ISSUE...DOUBLE CHECK TRENDS IN FILE AND DUPLICATE DATES AFTEWARD
+                # if (len(fiboSummary) >= 2):
+                #         currentFiboPair = fiboSummary[-1]
+                #         previousFiboPair = fiboSummary[-2]
+                #         if ((currentFiboPair.get("Low") == previousFiboPair.get("Low")) and (currentFiboPair.get("High") == previousFiboPair.get("High"))):
+                #                 foundDuplicate = True
+                #
+                # if (not foundDuplicate):
+                #         fiboSummary.append(fiboInfo)
+                #         foundDuplicate = False
                 fiboSummary.append(fiboInfo)
 
                 # Clear values for next Fibo Pair
@@ -111,19 +132,21 @@ while(running):
                         rowOfHighestHigh = EMPTY
                 elif (swingIsLowestLow):
                         rowOfLowestLow = EMPTY
-                
+
+
         if (endIndex == lastIndex):
                 running = False
 
 
 
 # Create name of file based on timeframe
-if (numberOfCandlesToView == MONTHLY):
+if (TIMEFRAME == 'MONTHLY'):
         csvName = 'monthly_results.csv'
-elif (numberOfCandlesToView == WEEKLY):
+elif (TIMEFRAME == 'WEEKLY'):
         csvName = 'weekly_results.csv'
-elif (numberOfCandlesToView == DAILY):
+elif (TIMEFRAME == 'DAILY'):
         csvName = 'daily_results.csv'
+
 
 # Write results to output file
 with open('../Output/' + csvName, 'w') as csvFile:
